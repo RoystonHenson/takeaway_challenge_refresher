fc64e7cbc26b7e32b445c48f18ecc15920a4080c
Royston Henson <rlqhenson@hotmail.com>
2018-03-16 03:10:22 +0000

    order can show menu

--- lib/order.rb
+++ lib/order.rb
@@ -5,4 +5,8 @@ class Order
     @menu = {'green curry': 6.95,
              'red curry':   6.95}
   end
+
+  def show_menu
+    @menu
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -6,4 +6,11 @@ describe Order do
       expect(subject.menu).to eq({'green curry': 6.95,
                                   'red curry':   6.95})
     end
+  end
+  describe '#show_menu' do
+    it 'lists the menu' do
+      expect(subject.show_menu).to eq({'green curry': 6.95,
+                                       'red curry':   6.95})
+    end
+  end
 end
