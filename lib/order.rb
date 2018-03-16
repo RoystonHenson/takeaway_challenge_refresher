6eee3f85a1f2e9bbb73b3f510b11758d3a67647c
Royston Henson <rlqhenson@hotmail.com>
2018-03-16 03:19:17 +0000

    adds a dish to an order

--- lib/order.rb
+++ lib/order.rb
@@ -1,12 +1,17 @@
 class Order
-  attr_reader :menu
+  attr_reader :menu, :order
 
   def initialize
+    @order = []
     @menu = {'green curry': 6.95,
              'red curry':   6.95}
   end
 
   def show_menu
     @menu
+  end
+
+  def select_dish(dish)
+    order << dish
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -6,11 +6,24 @@ describe Order do
       expect(subject.menu).to eq({'green curry': 6.95,
                                   'red curry':   6.95})
     end
+
+    it 'has an empty order' do
+      expect(subject.order).to be_empty
+    end
   end
+
   describe '#show_menu' do
     it 'lists the menu' do
       expect(subject.show_menu).to eq({'green curry': 6.95,
                                        'red curry':   6.95})
+    end
+  end
+
+  describe '#select_dish' do
+    it 'adds a dish to an order' do
+      dish = double(:dish, name: 'price')
+      subject.select_dish(dish)
+      expect(subject.order).to eq([dish])
     end
   end
 end
